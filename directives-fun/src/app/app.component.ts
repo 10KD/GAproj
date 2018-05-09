bdca828f9a0f62a635dfd762506a94fdb71e4190
10kd <mkim.don@gmail.com>
2018-05-09 14:45:30 -0700

    death square hw

---
+++ directives-fun/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pokemon = [
+    {
+      Ndex: 25,
+      name: 'Pikachu',
+      type: 'Electric'
+    },
+    {
+      Ndex: 10,
+      name: 'Caterpie',
+      type: 'Bug'
+    },
+    {
+      Ndex: 39,
+      name: 'Jigglypuff',
+      type: 'Fairy'
+    },
+    {
+      Ndex: 94,
+      name: 'Gengar',
+      type: 'Ghost'
+    },
+    {
+      Ndex: 143,
+      name: 'Snorlax',
+      type: 'Normal'
+    }
+  ];
+  title = 'app';
+  trainer = {name:'Ash'}
+
+  everySecond() { console.log('gotta catch em all'); }
+
+  onClick(){
+    console.log(this.trainer.name);
+  }
+}
