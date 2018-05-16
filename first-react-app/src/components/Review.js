cd37632da50bc21045f7bfd4389e6d1214c1b8b9
10kd <mkim.don@gmail.com>
2018-05-16 05:44:55 -0700

    bank of react

---
+++ first-react-app/src/components/Review.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Review extends React.Component {
+    render() {
+        return (
+
+        <div>
+            review
+        </div>
+        );
+    }
+}
+
+export default Review;
