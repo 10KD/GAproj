2fb503949e2337a3ed2a795693130c8d8eab68af
10kd <mkim.don@gmail.com>
2018-05-22 09:44:26 -0700

    started working on records-list component

---
+++ social-site/front-end/src/app/nyc-records-list/nyc-records-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nyc-records-list',
+  templateUrl: './nyc-records-list.component.html',
+  styleUrls: ['./nyc-records-list.component.css']
+})
+export class NycRecordsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ social-site/front-end/src/app/records.ts
@@ -0,0 +1,15 @@
+export const RECORDS = [
+    {
+        id: 3,
+        categoryName: 'Lebron',
+        
+    },
+    {
+        id: 2,
+        categoryName: 'james',
+    },
+    {
+        id: 1,
+        categoryName: 'lel',
+    }
+]
